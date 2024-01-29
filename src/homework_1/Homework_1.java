764eacc27ce7ed9174c3e195a3de85f44ee8741a
Ryan Wahle <ryanwahle8@gmail.com>
2024-01-29 15:15:24 -0600

    Added Ryan

--- src/homework_1/Homework_1.java
+++ src/homework_1/Homework_1.java
@@ -4,7 +4,10 @@ public class Homework_1 {
 
 	public static void main(String[] args) {
 		System.out.println("Zoe");
-		System.out.println("Darkskymoon - 108477682");
+		System.out.println("Darkskymoon - 108477682\n");
+		
+		System.out.println("Ryan");
+		System.out.println("Derbzzzzzz - 106635735\n6");
 
 	}
 
